9bd1b2bf797a1397dcbdcec53afe9e3c0e639116
Felipe Sumadots <felipe@sumadots.com>
2026-04-13 18:43:51 -0400

    fix: scraper with firefox

--- extraction_service/app/tms/qanalytics/scraper.py
+++ extraction_service/app/tms/qanalytics/scraper.py
@@ -54,15 +54,7 @@ class QAnalyticsExtractor(BaseTMSExtractor):
         os.makedirs(downloads_dir, exist_ok=True)
 
         async with async_playwright() as p:
-            browser = await p.chromium.launch(
-                headless=settings.BROWSER_HEADLESS,
-                args=[
-                    "--no-sandbox",
-                    "--disable-dev-shm-usage",
-                    "--disable-gpu",
-                    "--single-process",
-                ],
-            )
+            browser = await p.firefox.launch(headless=settings.BROWSER_HEADLESS)
             context = await browser.new_context(
                 accept_downloads=True,
                 ignore_https_errors=True,
